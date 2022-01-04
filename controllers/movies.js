8d41eb73fda4064c5aedfbb403828bab5ddf5037
Alina Smirnova <BlueOstrich@yandex.ru>
2022-01-04 17:19:16 +0300

    fix: возвращать только фильмы текущего пользователя

--- controllers/movies.js
+++ controllers/movies.js
@@ -4,7 +4,8 @@ const NotFoundError = require('../errors/not-found-error');
 const Movie = require('../models/movie');
 
 function findMovies(req, res, next) {
-  Movie.find({ })
+  const owner = req.user._id;
+  Movie.find({ owner })
     .then((movies) => res.send(movies))
     .catch(next);
 }
